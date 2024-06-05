3d46e4447920203d2a7985f32f41ef715f272a33
Johanna Botquin <johannabotquin@icloud.com>
2024-06-05 12:49:32 +0200

    action + view restaurant done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,10 +1,10 @@
 class Restaurant < ApplicationRecord
+  CATEGORIES = %w(chinese italian japanese french belgian)
+
   has_many :reviews, dependent: :destroy
 
   validates :name, presence: true
   validates :address, presence: true
   validates :phone_number, presence: true
-  validates :category, inclusion: { in: %w(chinese italian japanese french belgian)}
+  validates :category, presence: true, inclusion: { in: CATEGORIES }
 end
-
-# CATEGORIES = %w("chinese", "italian", "japanese", "french", "belgian")
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,20 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require "faker"
+# TODO: Write a seed to insert 100 posts in the database
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+
+5.times do
+  Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.phone_number_with_country_code,
+    category: Restaurant::CATEGORIES.sample
+  )
+end
+
+puts "Finished!"
